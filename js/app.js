be989e6ed17051c86238b06a22f8e450a6109671
mmorrii <byryshka55@gmail.com>
2023-03-14 13:48:46 +0200

    add functional change theme

--- js/app.js
+++ js/app.js
@@ -97,3 +97,22 @@ mailLink.addEventListener('dblclick', function (event) {
    event.preventDefault();
    linkText.classList.remove('show');
 });
+//-----------------------------------------------------------------------------------------------------------------------------------------
+let btnChangeTheme = document.querySelector('.theme');
+let wrapper = document.querySelector('.wrapper');
+var savedTheme = localStorage.getItem("theme");
+
+if (savedTheme === "light") {
+   wrapper.classList.add("light");
+}
+
+btnChangeTheme.addEventListener('click', () => {
+   wrapper.classList.toggle('light');
+
+   // сохраняем выбранную тему в локальном хранилище
+   if (wrapper.classList.contains("light")) {
+      localStorage.setItem("theme", "light");
+   } else {
+      localStorage.setItem("theme", "dark");
+   }
+})
